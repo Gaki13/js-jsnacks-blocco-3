a5dcd7a3d74627b84ce6d2c355d3210fa4d5289f
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-05 12:46:26 +0200

    chiedo i numeri fintantoché i valori sommati fanno 50 o più

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,15 +1,16 @@
 //1. Creo un'array vuoto
 const numbers = [];
 //2. Chiedi all'utente un numero da inserire nell'array
+//3. Continua a chiedere FINTANTOCHé la somma dei numeri all'interno è minore di 50
 let somma = 0;
 do {
     let userNumbers = Number(prompt("Inserisci un numero"));
     console.log (userNumbers);
-    let somma = userNumbers + userNumbers;
-    numbers.push (somma)
-} while (numbers < 50);
+    numbers.push (userNumbers);
+    somma += userNumbers 
+} while (somma < 50);
 
 console.log (numbers);
+console.log (somma);
 
 
-//3. Continua a chiedere FINTANTOCHé la somma dei numeri all'interno è minore di 50
