cc9e3f9e384b9b394410df06382b1368b0cc16e3
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-05 12:16:08 +0200

    creo un'array vuoto

---
+++ snack-1/main.js
@@ -0,0 +1,4 @@
+//1. Creo un'array vuoto
+const numbers = [];
+//2. Chiedi all'utente un numero da inserire nell'array
+//3. Continua a chiedere FINTANTOCHé la somma dei numeri all'interno è minore di 50
