51c74a2be6460678e2d0d0ffa34c127eb53299a5
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-05 12:32:04 +0200

    punto di incertezza

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,4 +1,15 @@
 //1. Creo un'array vuoto
 const numbers = [];
 //2. Chiedi all'utente un numero da inserire nell'array
+let somma = 0;
+do {
+    let userNumbers = Number(prompt("Inserisci un numero"));
+    console.log (userNumbers);
+    let somma = userNumbers + userNumbers;
+    numbers.push (somma)
+} while (numbers < 50);
+
+console.log (numbers);
+
+
 //3. Continua a chiedere FINTANTOCHé la somma dei numeri all'interno è minore di 50
