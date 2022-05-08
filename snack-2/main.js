01440d47b012c225f72d216beede626616dbea47
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-08 22:35:34 +0200

    creo N array

--- snack-2/main.js
+++ snack-2/main.js
@@ -2,4 +2,12 @@
 number = Number(prompt("Inserisci il numero di array che vuoi creare"));
 
 //2. Genera N array, ognuno composto da 10 numeri tra 1 e 100
+for ( let i = 0; i < number; i++ ){
+    const arr = [];
+    for ( let i = 0; i < 10; i++ ){
+        const number = Math.floor(Math.random() * 100) + 1;
+        arr.push(number);
+    }
+}
+
 //3. Ogni array creato, stampalo a schermo
